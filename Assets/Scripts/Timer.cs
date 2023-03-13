0a56b31005463c51f31598a7e75e3aa3998818a6
CoderValencia <valencia.coleman625@gmail.com>
2023-03-13 02:58:52 -0400

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime;
+    public TextMeshProUGUI currentTimeText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = currentTime + Time.deltaTime;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(currentTime);
+
+        currentTimeText.text = timeSpan.ToString(@"mm\:ss");
+
+    }
+}
